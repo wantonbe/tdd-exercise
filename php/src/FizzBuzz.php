502176ba7008124a69876b6ee11cfcc4d96d2992
wantonbe <wantonbe@gmail.com>
2023-05-25 23:56:24 +0900

    FizzBuzzの仮実装

---
+++ php/src/FizzBuzz.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class FizzBuzz
+{
+    public function execute()
+    {
+        return 1;
+    }
+}
---
+++ php/tests/FizzBuzzTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use App\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+    /** @test */
+    public function 入力値1の場合は1を返す()
+    {
+        $fizz_buzz = new FizzBuzz();
+        $this->assertSame(1, $fizz_buzz->execute(1));
+    }
+}
