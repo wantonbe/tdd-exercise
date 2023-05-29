45aa758312fec746af3047757181b06dc38085c3
wantonbe <wantonbe@gmail.com>
2023-05-29 23:48:08 +0900

    :beers: FizzBuzzで扱う数値を表現するクラスを定義した

---
+++ php/src/FizzBuzzNumber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class FizzBuzzNumber
+{
+    public function __construct(private int $number)
+    {
+    }
+
+    public function isDivisibleBy(int $number)
+    {
+        return $this->number % $number === 0;
+    }
+}
---
+++ php/tests/FizzBuzzNumberTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+use App\FizzBuzzNumber;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers FizzBuzzNumber
+ */
+class FizzBuzzNumberTest extends TestCase
+{
+    /** @test */
+    public function isDivisibleBy__指定の数値で割り切れる場合はtrueを返す()
+    {
+        $number = new FizzBuzzNumber(100);
+        $this->assertTrue($number->isDivisibleBy(5));
+    }
+
+    /** @test */
+    public function isDivisibleBy__指定の数値で割り切れない場合はfalseを返す()
+    {
+        $number = new FizzBuzzNumber(100);
+        $this->assertFalse($number->isDivisibleBy(3));
+    }
+}
